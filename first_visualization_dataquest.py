c4ca6c5d9772b0f538b732c2a9d8af8cd573799d
Dongliang (Larry) Yi <dongliang.yi@aol.com>
2020-09-02 20:21:27 -0700

    test commit from GitHub app

---
+++ first_visualization_dataquest.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep  2 20:18:29 2020
+
+@author: larry
+"""
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+women_degrees = pd.read_csv('percent-bachelors-degrees-women-usa.csv')
+cb_dark_blue = (0/255,107/255,164/255)
+cb_orange = (255/255, 128/255, 14/255)
+
+stem_cats = ['Psychology', 'Biology', 'Math and Statistics', 'Physical Sciences', 'Computer Science', 'Engineering']
+lib_arts_cats = ['Foreign Languages', 'English', 'Communications and Journalism', 'Art and Performance', 'Social Sciences and History']
+other_cats = ['Health Professions', 'Public Administration', 'Education', 'Agriculture','Business', 'Architecture']
+
+fig = plt.figure(figsize=(9, 18))
+
+#stem
+for sp in range(0,6):
+    ax = fig.add_subplot(6,3,sp*3+1)
+    ax.plot(women_degrees['Year'], women_degrees[stem_cats[sp]], c=cb_dark_blue, label='Women', linewidth=3)
+    ax.plot(women_degrees['Year'], 100-women_degrees[stem_cats[sp]], c=cb_orange, label='Men', linewidth=3)
+    ax.spines["right"].set_visible(False)    
+    ax.spines["left"].set_visible(False)
+    ax.spines["top"].set_visible(False)    
+    ax.spines["bottom"].set_visible(False)
+    ax.set_xlim(1968, 2011)
+    ax.set_ylim(0,100)
+    ax.set_title(stem_cats[sp])
+    ax.tick_params(bottom="off", top="off", left="off", right="off", labelbottom='off')
+    ax.set_yticks([0,100])
+    ax.axhline(50, c=(171/255, 171/255, 171/255), alpha=0.3)
+    
+    if sp == 0:
+        ax.text(2005, 10, 'Men')
+        ax.text(2002, 85, 'Women')
+    elif sp == 5:
+        ax.text(2005, 85, 'Men')
+        ax.text(2001, 6, 'Women')
+        ax.tick_params(labelbottom='on')
+
+#liberal arts
+for sp in range(0,5):
+    ax = fig.add_subplot(6,3,sp*3+2)
+    ax.plot(women_degrees['Year'], women_degrees[lib_arts_cats[sp]], c=cb_dark_blue, label='Women', linewidth=3)
+    ax.plot(women_degrees['Year'], 100-women_degrees[lib_arts_cats[sp]], c=cb_orange, label='Men', linewidth=3)
+    ax.spines["right"].set_visible(False)    
+    ax.spines["left"].set_visible(False)
+    ax.spines["top"].set_visible(False)    
+    ax.spines["bottom"].set_visible(False)
+    ax.set_xlim(1968, 2011)
+    ax.set_ylim(0,100)
+    ax.set_title(lib_arts_cats[sp])
+    ax.tick_params(bottom="off", top="off", left="off", right="off", labelbottom='off')
+    ax.set_yticks([0,100])
+    ax.axhline(50, c=(171/255, 171/255, 171/255), alpha=0.3)
+    
+    if sp == 0:
+        ax.text(2005, 15, 'Men')
+        ax.text(2002, 75, 'Women')
+    elif sp == 4:
+        ax.tick_params(labelbottom='on')
+
+#other
+for sp in range(0,6):
+    ax = fig.add_subplot(6,3,sp*3+3)
+    ax.plot(women_degrees['Year'], women_degrees[other_cats[sp]], c=cb_dark_blue, label='Women', linewidth=3)
+    ax.plot(women_degrees['Year'], 100-women_degrees[other_cats[sp]], c=cb_orange, label='Men', linewidth=3)
+    ax.spines["right"].set_visible(False)    
+    ax.spines["left"].set_visible(False)
+    ax.spines["top"].set_visible(False)    
+    ax.spines["bottom"].set_visible(False)
+    ax.set_xlim(1968, 2011)
+    ax.set_ylim(0,100)
+    ax.set_title(other_cats[sp])
+    ax.tick_params(bottom="off", top="off", left="off", right="off", labelbottom='off')
+    ax.set_yticks([0,100])
+    ax.axhline(50, c=(171/255, 171/255, 171/255), alpha=0.3)
+    
+    if sp == 0:
+        ax.text(2005, 5, 'Men')
+        ax.text(2002, 90, 'Women')
+    elif sp == 5:
+        ax.text(2005, 55, 'Men')
+        ax.text(2001, 30, 'Women')
+        ax.tick_params(labelbottom='on')
+        
+plt.show()
+fig.savefig('degree_gap.png')
